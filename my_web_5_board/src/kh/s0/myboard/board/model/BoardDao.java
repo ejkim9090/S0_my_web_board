59abc2f35a7fc3d4b4fd64d0d3864084ea522389
ejkim0909 <ej.kb.kim@gmail.com>
2022-12-08 22:03:51 +0900

    EncodingFilter, Board VoDaoService CRUD기본메소드

--- my_web_5_board/src/kh/s0/myboard/board/model/BoardDao.java
+++ my_web_5_board/src/kh/s0/myboard/board/model/BoardDao.java
@@ -1,5 +1,42 @@
 package kh.s0.myboard.board.model;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+
 public class BoardDao {
+// 최소 5개 
+//	insert
+	public int insert(Connection conn, BoardVo vo) {
+		int result = 0;
+		return result;
+	}
+//	update
+	public int update(Connection conn, BoardVo vo, int bno/*주로 PK*/) {
+		int result = 0;
+		return result;
+	}
+//	delete
+	public int delete(Connection conn, int bno/*주로 PK*/) {
+		int result = 0;
+		return result;
+	}
+//	selectList
+	public List<BoardVo> selectList(Connection conn){
+		List<BoardVo> volist = null;
+//		Statement stmt = conn.createStatement();
+//		String sql = "select * from board";
+//		ResultSet rs = stmt.executeQuery(sql);
+//		while(rs.next()) {
+//			
+//		}
+		return volist;
+	}
+//	selectOne
+	public BoardVo selectOne(Connection conn, int bno/*주로 PK*/){
+		BoardVo vo = null;
+		return vo;
+	}
 
 }
--- my_web_5_board/src/kh/s0/myboard/board/model/BoardVo.java
+++ my_web_5_board/src/kh/s0/myboard/board/model/BoardVo.java
@@ -1,6 +1,7 @@
 package kh.s0.myboard.board.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 public class BoardVo {
 //	BNO      NOT NULL NUMBER         
@@ -13,7 +14,139 @@ public class BoardVo {
 //	BRESTEP  NOT NULL NUMBER         
 //	BWRITER  NOT NULL VARCHAR2(30)   
 //	BTYPE    NOT NULL CHAR(1)   
+	
+//	<List>
+//	BFILEPATH NOT NULL VARCHAR2(500) 
+//	BNO       NOT NULL NUMBER    
+	
+	private int bno;
+	private String btitle;
+	private String bcontent;
 	private Timestamp bdate;
+	private int bcnt;
+	private int bref;
+	private int brelevel;
+	private int brestep;
+	private String bwriter;
+	private String btype;
+	
+// bfile 테이블에 대한 방법 1
+	private List<BoardFileVo> bfilelist;
+
+	@Override
+	public String toString() {
+		return "BoardVo [bno=" + bno + ", btitle=" + btitle + ", bcontent=" + bcontent + ", bdate=" + bdate + ", bcnt="
+				+ bcnt + ", bref=" + bref + ", brelevel=" + brelevel + ", brestep=" + brestep + ", bwriter=" + bwriter
+				+ ", btype=" + btype + ", bfilelist=" + bfilelist + "]";
+	}
+
+	public int getBno() {
+		return bno;
+	}
+
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+
+	public String getBtitle() {
+		return btitle;
+	}
+
+	public void setBtitle(String btitle) {
+		this.btitle = btitle;
+	}
+
+	public String getBcontent() {
+		return bcontent;
+	}
+
+	public void setBcontent(String bcontent) {
+		this.bcontent = bcontent;
+	}
+
+	public Timestamp getBdate() {
+		return bdate;
+	}
+
+	public void setBdate(Timestamp bdate) {
+		this.bdate = bdate;
+	}
+
+	public int getBcnt() {
+		return bcnt;
+	}
+
+	public void setBcnt(int bcnt) {
+		this.bcnt = bcnt;
+	}
+
+	public int getBref() {
+		return bref;
+	}
+
+	public void setBref(int bref) {
+		this.bref = bref;
+	}
+
+	public int getBrelevel() {
+		return brelevel;
+	}
+
+	public void setBrelevel(int brelevel) {
+		this.brelevel = brelevel;
+	}
+
+	public int getBrestep() {
+		return brestep;
+	}
+
+	public void setBrestep(int brestep) {
+		this.brestep = brestep;
+	}
+
+	public String getBwriter() {
+		return bwriter;
+	}
+
+	public void setBwriter(String bwriter) {
+		this.bwriter = bwriter;
+	}
+
+	public String getBtype() {
+		return btype;
+	}
+
+	public void setBtype(String btype) {
+		this.btype = btype;
+	}
+
+	public List<BoardFileVo> getBfilelist() {
+		return bfilelist;
+	}
+
+	public void setBfilelist(List<BoardFileVo> bfilelist) {
+		this.bfilelist = bfilelist;
+	}
+	
+// bfile 테이블에 대한 방법 2
+//	private List<String> bfilepathlist;
+	
 	
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
